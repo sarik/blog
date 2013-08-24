2fe74c88cc703e3d8d206fbe5d57cc01487510f0
sarik <siddiquisarik@gmail.com>
2013-08-24 10:13:26 +0530

    micropost validated for length

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130823011051) do
+ActiveRecord::Schema.define(version: 20130824042722) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
